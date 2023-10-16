941eb70b64eefad40145b0b2d89739183ec3e6f6
Ian Lucas Oliveira Bastos <ian.bastos@el.com.br>
2023-10-16 19:43:04 -0300

    feat: adding pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ export default defineNuxtConfig({
   ssr: true,
   devtools: { enabled: true },
   build: { transpile: ["vuetify"] },
+  modules: ["@vite-pwa/nuxt"],
+  pwa: {
+    manifest: {
+      name: "Matrix",
+      short_name: "Mtx",
+      description: "Sistema para realizar cálculos envolvendo matrizes",
+      background_color: "#1234",
+    },
+  },
 });
